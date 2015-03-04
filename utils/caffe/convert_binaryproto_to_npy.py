6ea84c92e63257878efcdf0ae7d6b04aec70d1a1
Mikhail Sushkov <sushkovm@gmail.com>
2015-03-03 23:52:04 -0800

    indentation error

--- utils/caffe/convert_binaryproto_to_npy.py
+++ utils/caffe/convert_binaryproto_to_npy.py
@@ -10,12 +10,11 @@ import numpy as np
 # Converts a file in binaryproto format to a file in nby format. (Called by make_mean.sh)
 
 if len(sys.argv) != 3:
-	print "Usage: python convert_binaryproto_to_npy.py file.binaryproto out.npy"
-    sys.exit()
-
-blob = caffe.proto.caffe_pb2.BlobProto()
-data = open(sys.argv[1], 'rb').read()
-blob.ParseFromString(data)
-arr = np.array(caffe.io.blobproto_to_array(blob))
-out = arr[0]
-np.save( sys.argv[2], out)
+    print "Usage: python convert_binaryproto_to_npy.py file.binaryproto out.npy"
+else:
+    blob = caffe.proto.caffe_pb2.BlobProto()
+    data = open(sys.argv[1], 'rb').read()
+    blob.ParseFromString(data)
+    arr = np.array(caffe.io.blobproto_to_array(blob))
+    out = arr[0]
+    np.save( sys.argv[2], out)

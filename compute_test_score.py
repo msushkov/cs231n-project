21fda7d38f220f7493fc970dcb011ef4eada35a1
Mikhail Sushkov <sushkovm@gmail.com>
2015-03-05 22:57:29 -0800

    compute top k scores

--- compute_test_score.py
+++ compute_test_score.py
@@ -14,6 +14,7 @@ MODEL_FILE = os.environ['MODEL_FILE']
 PRETRAINED = os.environ['PRETRAINED_NETWORK']
 MEAN_FILE = os.environ['MEAN_FILE_NPY']
 GROUND_TRUTH_LABEL_FILE = os.environ['TEST_LABELS']
+K = 2 # take the top k when computing score
 
 caffe.set_mode_gpu()
 net = caffe.Classifier(MODEL_FILE, PRETRAINED, mean=np.load(MEAN_FILE).mean(1).mean(1), channel_swap=(2, 1, 0), raw_scale=255)
@@ -42,10 +43,19 @@ errors = Counter()
 totals = Counter()
 for i, filename in enumerate(image_filenames):
 	curr_predictions = predictions[i]
-	best_prediction = curr_predictions.argmax()
+
+	idx = [i for i in xrange(len(curr_predictions))]
+	preds = zip(curr_predictions, idx) # gives list of tuples (pred, index)
+
+	sorted_predictions = sorted(preds, reverse=True)
+	top_k = sorted_predictions[:K]
+
+	top_k_labels = set([index for (pred, index) in top_k])
+
 	true_label = image_labels[filename]
+
 	totals[true_label] += 1
-	if true_label != best_prediction:
+	if true_label not in top_k_labels:
 		errors[true_label] += 1
 
 print "Accuracy: "

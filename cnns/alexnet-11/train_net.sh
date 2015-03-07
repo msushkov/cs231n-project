e06d00bc05db291b1f3210f7e5d716d7e0f6282e
Mikhail Sushkov <sushkovm@gmail.com>
2015-03-06 16:35:36 -0800

    update train_net.sh

--- cnns/alexnet-11/train_net.sh
+++ cnns/alexnet-11/train_net.sh
@@ -2,4 +2,10 @@
 
 REFERENCE_MODEL=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
 
-.$CAFFE_ROOT/build/tools/caffe train -solver solver.prototxt -weights $REFERENCE_MODEL -gpu 0
+dir=$(pwd)
+
+cd $CAFFE_ROOT
+
+./build/tools/caffe train -solver solver.prototxt -weights $REFERENCE_MODEL -gpu 0
+
+cd dir

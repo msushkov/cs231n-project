d2b77e864273a612122bd27e24ec3f22d76efb9e
Mikhail Sushkov <sushkovm@gmail.com>
2015-03-10 21:47:59 -0700

    removed small (under 3kb) image files

--- utils/remove_zero_kb_files.py
+++ utils/remove_zero_kb_files.py
@@ -5,11 +5,14 @@ import os
 # Removes JPG files that are less than 3kb in size from the subdirectories (1 level down) in th
 # given directory.
 
-# path relative to the directory where this script lives
-DIR = "../data/images/imagenet11/256/val"
+# paths relative to the directory where this script lives
+DIRS = [
+	"../data/images/train/no_augmentations/imagenet/256",
+	"../data/images/val/instagram/227"
+]
+
 SIZE = 3000
 
-directory = os.path.join(os.getcwd(), DIR)
 
 # Returns a list of full directory paths in current directory.
 def get_dirs_in_curr(curr_dir):
@@ -19,14 +22,19 @@ def get_dirs_in_curr(curr_dir):
 def get_image_filenames(dir_path):
 	return [os.path.join(dir_path, o) for o in os.listdir(dir_path) if o.lower().endswith(".jpeg") or o.lower().endswith(".jpg")]
 
-count = 0
-dirs = get_dirs_in_curr(directory)
-for directory in dirs:
-	image_filenames = get_image_filenames(directory)
-	for full_file in image_filenames:
-		filesize = os.path.getsize(full_file)
-		if filesize < SIZE:
-			os.remove(full_file)
-			count += 1
 
-print "Removed " + str(count) + " files."
+for DIR in DIRS:
+	print DIR
+
+	directory = os.path.join(os.getcwd(), DIR)
+	count = 0
+	dirs = get_dirs_in_curr(directory)
+	for directory in dirs:
+		image_filenames = get_image_filenames(directory)
+		for full_file in image_filenames:
+			filesize = os.path.getsize(full_file)
+			if filesize < SIZE:
+				os.remove(full_file)
+				count += 1
+
+	print "Removed " + str(count) + " files."

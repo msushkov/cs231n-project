4e2e3fbcbfe4d328b76ed9b8fe6dfac550845066
Mikhail Sushkov <sushkovm@gmail.com>
2015-03-15 19:34:14 -0700

    asdf

--- cnns/baseline_cnn/train_net.sh
+++ cnns/baseline_cnn/train_net.sh
@@ -6,6 +6,6 @@ LOG_FILE=$dir/train_output.txt
 
 cd $CAFFE_ROOT
 
-./build/tools/caffe train -solver $dir/solver.prototxt -gpu 0 > $LOG_FILE
+./build/tools/caffe train -solver $dir/solver.prototxt -gpu 0 2> $LOG_FILE
 
 cd $dir
